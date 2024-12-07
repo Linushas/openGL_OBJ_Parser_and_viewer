4703a601cce0b0b4c63932b995732819abefec90
Linus <linus.hasselkvist@gmail.com>
2024-12-07 19:06:25 +0100

    glew and openGL remake

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
 CC = gcc
-CFLAGS = -Isrc/Include 
-LDFLAGS = -Lsrc/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -Wall
-SRC = src/main.c
-OBJ = $(SRC:.c=.o)
+CFLAGS = -Isrc/SDL2/include -Isrc/GLEW/include
+LDFLAGS = -Lsrc/SDL2/lib -Lsrc/GLEW/lib/Release/x64 -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lglew32 -lopengl32 -Wall
 
-main: $(OBJ)
+SRC = src/main.c
+BUILD_DIR = src/build
+OBJ = $(SRC:src/%.c=$(BUILD_DIR)/%.o)
+TARGET = $(BUILD_DIR)/main.exe
+
+all: $(TARGET)
+
+$(TARGET): $(OBJ)
 	$(CC) -o $@ $^ $(LDFLAGS)
 
-%.o: %.c
+$(BUILD_DIR)/%.o: src/%.c
+	if not exist $(BUILD_DIR) mkdir $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
+run: all
+	$(TARGET)
+
 clean:
-	del /Q $(subst /,\,$(OBJ)) main.exe
+	del /Q $(subst /,\,$(OBJ)) $(subst /,\,$(TARGET))
--- src/main.c
+++ src/main.c
@@ -1,52 +1,82 @@
+/*
+ * 3D engine made with SDL2 and C language
+ * Author: Linus Hasselkvist
+ * 2024-12-07
+ * 
+ * */
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
+#include <GL/glew.h>
 
-#define SCREEN_HEIGHT 1080 *2/3
-#define SCREEN_WIDTH SCREEN_HEIGHT *16/9
-#define FPS 60
-
+#define res         4               // 0=160*X 1=360*X 4=640*X ...
+#define aspectRatio 16/9
+#define SH          160*res
+#define SW          SH*aspectRatio
+#define SH2         SH/2
+#define SW2         SW/2
+#define FPS         60  
+//-----------------------------------------------------------------------
 typedef struct eventHandler {
     SDL_Event event;
     int running;
     int fullScreen;
 } EventH;
 
-void render(SDL_Renderer *rend);
+void render();
 void getWindowEvents(EventH *eh, SDL_Window *win);
 void toggleFullscreen(EventH *eh, SDL_Window *win);
 
 int main(int argc, char *argv[]) {
-
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("SDL2 3D Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    SDL_Renderer *rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    EventH eh = {.running = 1, .fullScreen = 0};
+    SDL_Window *win = SDL_CreateWindow("SDL2 3D Engine", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+        SW, SH, SDL_WINDOW_OPENGL);
 
-    while(eh.running) {
-        getWindowEvents(&eh, win);
-
-        render(rend);
-
-        SDL_Delay(1000/FPS);
+    SDL_GLContext glContext = SDL_GL_CreateContext(win);
+    if (!glContext) {
+        printf("Kunde inte skapa OpenGL-kontext: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return -1;
     }
 
-    SDL_DestroyRenderer(rend);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        printf("Kunde inte initiera GLEW\n");
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return -1;
+    }
+
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+    EventH eh = {.running = 1, .fullScreen = 0};
+
+    // MAIN LOOP
+    while(eh.running) {
+        getWindowEvents(&eh, win);
+        render();
+        SDL_GL_SwapWindow(win);
+    }
+
+    SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
     return 0;
 }
 
-void render(SDL_Renderer *rend) {
-    SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
-    SDL_RenderClear(rend);
-
-    SDL_RenderPresent(rend);
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
 }
 
 void getWindowEvents(EventH *eh, SDL_Window *win) {
